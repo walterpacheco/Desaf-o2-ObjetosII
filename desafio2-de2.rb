7caaf4255d6c520b5c389068df82121f37a43a2f
Walter Pacheco <walter.e.pacheco.chandia@gmail.com>
2019-05-22 15:30:08 -0400

    Ejercicio 1

---
+++ desafio2-de2.rb
@@ -0,0 +1,14 @@
+
+class Anything
+    attr_reader :a
+    def foo
+         @a = 5
+    end
+    def bar  
+         @a+= 1
+    end
+end
+    any = Anything.new
+    any.foo
+    any.bar
+    any.a
